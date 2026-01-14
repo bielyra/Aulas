5e3b7d2dd6e4ff1b2f75ec0441efa3995fec516e
Gabriel <br.gabrielyra@gmail.com>
2026-01-14 11:59:07 -0300

    aula 01

---
+++ script.js
@@ -0,0 +1,67 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+/*
+var scroll = 1000;
+ scroll += 500;
+ console.log(scroll);
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+if(possuiCarro && possuiCasa === true){
+    darCredito = true;
+}else{
+    darCredito = false;
+}
+
+console.log(darCredito);
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
+}
+
+// Como corrigir o erro abaixo?
+function somarDois(x) {
+  const dois = 2;
+  return x + dois;
+}
+function dividirDois(x) {
+  const dois = 2;
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+
+*/
+
+// Retorne o url da página atual utilizando o objeto window
+const hrefPagina = window.location.href;
+console.log(hrefPagina);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+
+// Retorne a largura da janela 
+console.log(innerWidth);
