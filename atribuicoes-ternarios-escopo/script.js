b13e6fcb264d9852bdf0bf2518aac5cc3e350f51
Gabriel <br.gabrielyra@gmail.com>
2026-01-14 11:10:35 -0300

    Finalização do modulo JS para Iniciantes

--- atribuicoes-ternarios-escopo/script.js
+++ atribuicoes-ternarios-escopo/script.js
@@ -6,6 +6,7 @@ var scroll = 1000;
 // Atribua true para a variável darCredito,
 // caso o cliente possua carro e casa.
 // E false caso o contrário.
+/*
 var possuiCarro = true;
 var possuiCasa = true;
 var darCredito;
@@ -17,3 +18,37 @@ if(possuiCarro && possuiCasa === true){
 }
 
 console.log(darCredito);
+*/
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
+}
+
+// Como corrigir o erro abaixo?
+function somarDois(x) {
+    const dois = 2;
+    return x + dois;
+}
+function dividirDois(x) {
+    const dois = 2;
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(6));
+
+// O que fazer para total retornar 500?
+var numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
